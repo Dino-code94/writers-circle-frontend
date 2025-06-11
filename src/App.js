29c578f3ab150cc3cf761be73e831a9a69a21337
Dino-code94 <dinoklaric29@gmail.com>
2025-06-11 10:43:47 -0700

    Frontend: added AdminDashboard route with comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import core components
 import Navbar from './components/Navbar';
 import Register from './components/Register';
 import Login from './components/Login';
 import PostList from './components/PostList';
 import CreatePost from './components/CreatePost';
-import PostDetail from './components/PostDetail';  // full import here
+import PostDetail from './components/PostDetail';
+
+// Import Admin Dashboard
+import AdminDashboard from './components/AdminDashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<PostList />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create" element={<CreatePost />} />
-        <Route path="/post/:id" element={<PostDetail />} />
+        {/* Public Routes */}
+        <Route path="/" element={<PostList />} />               {/* Home - List posts */}
+        <Route path="/register" element={<Register />} />        {/* User Registration */}
+        <Route path="/login" element={<Login />} />              {/* User Login */}
+        <Route path="/create" element={<CreatePost />} />        {/* Create new post */}
+        <Route path="/post/:id" element={<PostDetail />} />      {/* View post details */}
+
+        {/* Admin Route */}
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />  {/* Admin Dashboard */}
       </Routes>
     </BrowserRouter>
   );
