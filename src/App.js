03b0b700f4b9f34eb9effac2a17a29994e375032
Dino-code94 <dinoklaric29@gmail.com>
2025-06-11 08:03:21 -0700

    Wire full frontend routing with PostDetail page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import Login from './components/Login';
+import PostList from './components/PostList';
+import CreatePost from './components/CreatePost';
+import PostDetail from './components/PostDetail';  // full import here
+
 function App() {
   return (
-    <div>
-      <h1>Writer's Circle Frontend</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/post/:id" element={<PostDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
